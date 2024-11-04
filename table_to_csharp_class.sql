a22af47b927462bb9c072be3cf31b2ac703b202a
DEV-DIEGOC-107\Diego <diego.visualsoftware@gmail.com>
2024-11-04 09:44:58 -0300

    start repo

---
+++ table_to_csharp_class.sql
@@ -0,0 +1,27 @@
+DECLARE @TABLE_NAME nvarchar(MAX) =  'PRODUTO';
+DECLARE @CLASS_NAME  nvarchar(MAX) = 'PRODUTO';
+DECLARE @PREFIX nvarchar(10) = 'PRO_';
+
+SET NOCOUNT ON
+SELECT '[Table("'+ @TABLE_NAME + '")]' + CHAR(13) + 
+		'public class ' + @CLASS_NAME + '{' + CHAR(13)
+UNION ALL
+SELECT	
+			concat('[Column("',COLUMN_NAME,'")] ' + CHAR(13),
+			  'public ', 
+			  case DATA_TYPE
+				when 'nvarchar' then 'string' 
+				when 'varchar' then 'string'
+				when 'datetime' then 'DateTime'
+				when 'money' then 'decimal' 
+
+				else DATA_TYPE
+			  end,
+			  ' ', 
+			  REPLACE(COLUMN_NAME, @PREFIX, ''), 
+			  '{get; set; }', CHAR(13)
+			  )
+	FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = @TABLE_NAME
+union all
+select '}';
