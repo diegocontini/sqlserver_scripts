050038d213cda5f5b3d13dc94ae7372a17117da1
DEV-DIEGOC-107\Diego <diego.visualsoftware@gmail.com>
2025-06-04 09:36:06 -0300

    feat: add decimal precision when the datatype is decimal

--- table_to_csharp_class_with_case_modifier.sql
+++ table_to_csharp_class_with_case_modifier.sql
@@ -1,5 +1,5 @@
-DECLARE @TABLE_NAME nvarchar(MAX) =  'ROTA';
-DECLARE @CLASS_NAME  nvarchar(MAX) = 'Rota';
+DECLARE @TABLE_NAME nvarchar(MAX) =  'venda';
+DECLARE @CLASS_NAME  nvarchar(MAX) = 'venda';
 
 -- Create a function to transform the attributes to PascalCase instead of snake_case.
 -- Example: PRO_DESCRICAO_COMPLEMENTAR becomes DescricaoComplementar
@@ -52,19 +52,27 @@ END;
 CREATE TABLE #TempColumns (
     COLUMN_NAME nvarchar(128),
     DATA_TYPE nvarchar(128),
-    IS_NULLABLE nvarchar(3)
+    IS_NULLABLE nvarchar(3),
+    NUMERIC_PRECISION int,
+    NUMERIC_SCALE int
 );
 
-INSERT INTO #TempColumns (COLUMN_NAME, DATA_TYPE, IS_NULLABLE)
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE
+INSERT INTO #TempColumns (COLUMN_NAME, DATA_TYPE, IS_NULLABLE, NUMERIC_PRECISION, NUMERIC_SCALE)
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, NUMERIC_PRECISION, NUMERIC_SCALE
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = @TABLE_NAME;
 
 SELECT '[Table("' + @TABLE_NAME + '")]' + CHAR(13) + 
        'public class ' + @CLASS_NAME + ' {' + CHAR(13)
 UNION ALL
-SELECT concat(
-              '[Column("', COLUMN_NAME, '")] ' + CHAR(13),
+
+SELECT CONCAT(
+              '[Column(',
+              CASE DATA_TYPE
+                  WHEN 'decimal' THEN 'TypeName = "decimal(' + CAST(NUMERIC_PRECISION AS nvarchar) + ', ' + CAST(NUMERIC_SCALE AS nvarchar) + ')"'
+                  ELSE '"' + COLUMN_NAME + '"'
+              END,
+              ')]' + CHAR(13),
               'public ', 
               CASE DATA_TYPE
                   WHEN 'nvarchar' THEN 'string' 
@@ -73,11 +81,11 @@ SELECT concat(
                   WHEN 'money' THEN 'decimal' 
                   WHEN 'bit' THEN 'bool'
                   WHEN 'smallint' THEN 'short'
-                  WHEN 'bigint' then 'long'
+                  WHEN 'bigint' THEN 'long'
                   WHEN 'float' THEN 'double'
-                  WHEN 'date' then 'DateTime'
-                  WHEN 'tinyint' then 'byte'
-                  WHEN  'uniqueidentifier' then 'Guid'
+                  WHEN 'date' THEN 'DateTime'
+                  WHEN 'tinyint' THEN 'byte'
+                  WHEN 'uniqueidentifier' THEN 'Guid'
                   ELSE DATA_TYPE
               END,
               CASE IS_NULLABLE
@@ -92,6 +100,6 @@ FROM #TempColumns
 UNION ALL
 SELECT '}';
 
--- Drop the temporary table and the function
+-- Cleanup
 DROP TABLE #TempColumns;
 DROP FUNCTION SnakeToPascal;
